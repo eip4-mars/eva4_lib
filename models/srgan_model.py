7a6884cf0c3b8a39d7bca9a1778def4965a109cb
eip4-mars <68113773+eip4-mars@users.noreply.github.com>
2020-10-02 16:22:42 +0530

    Update srgan_model.py

--- models/srgan_model.py
+++ models/srgan_model.py
@@ -10,7 +10,7 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.block1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=9, padding=4),
-            nn.PReLU()
+            nn.ReLU()
         )
         self.block2 = ResidualBlock(64)
         self.block3 = ResidualBlock(64)
@@ -43,39 +43,39 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.net = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
 
             nn.AdaptiveAvgPool2d(1),
             nn.Conv2d(512, 1024, kernel_size=1),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(),
             nn.Conv2d(1024, 1, kernel_size=1)
         )
 
@@ -89,14 +89,14 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(channels)
-        self.prelu = nn.PReLU()
+        self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(channels)
 
     def forward(self, x):
         residual = self.conv1(x)
         residual = self.bn1(residual)
-        residual = self.prelu(residual)
+        residual = self.relu(residual)
         residual = self.conv2(residual)
         residual = self.bn2(residual)
 
@@ -108,10 +108,10 @@ class UpsampleBLock(nn.Module):
         super(UpsampleBLock, self).__init__()
         self.conv = nn.Conv2d(in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1)
         self.pixel_shuffle = nn.PixelShuffle(up_scale)
-        self.prelu = nn.PReLU()
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.conv(x)
         x = self.pixel_shuffle(x)
-        x = self.prelu(x)
+        x = self.relu(x)
         return x
